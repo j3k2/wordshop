eb946a1601cab2ab0e44cad1dfba34d3e64edeca
jeikei <johnkim512@gmail.com>
2014-11-25 11:02:11 -0800

    users/session/auth complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  helper_method :current_user
+  
+  def login(user)
+    user.reset_session_token
+    session[:session_token] = user.session_token
+  end
+  
+  def current_user
+    return nil if session[:session_token].nil?
+    current_user = User.find_by_session_token(session[:session_token])
+  end
+  
+  def require_current_user
+    redirect_to new_session_url unless current_user
+  end
 end
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  
+  before_action :require_current_user
+
+  def root
+  end
+end
