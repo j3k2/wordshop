9c25d1476c7abfbb4622c5b1f4e0dbb2f7d57101
jeikei <johnkim512@gmail.com>
2014-12-01 11:43:55 -0800

    UserShow has tabs working for Texts and Critiques

--- app/assets/javascripts/views/users/user_show.js
+++ app/assets/javascripts/views/users/user_show.js
@@ -5,18 +5,23 @@ Wordshop.Views.UserShow = Backbone.CompositeView.extend({
 	initialize: function(){
 		this.listenTo(this.model, 'sync', this.render);
 		
-		var UserTextsIndexView = new Wordshop.Views.UserTextsIndex({
-			collection: this.model.texts()
-		});
-		
-		this.addSubview('#user-items-list', UserTextsIndexView);
-		
-		var UserCritsIndexView = new Wordshop.Views.UserCritsIndex({
-			collection: this.model.critiques()
-		});
-		
-		this.addSubview('#user-items-list', UserCritsIndexView);
-		
+		// var UserTextsIndexView = new Wordshop.Views.UserTextsIndex({
+		// 	collection: this.model.texts()
+		// });
+		//
+		// this.addSubview('#user-items-list', UserTextsIndexView);
+		//
+		// var UserCritsIndexView = new Wordshop.Views.UserCritsIndex({
+		// 	collection: this.model.critiques()
+		// });
+		//
+		// this.addSubview('#user-items-list', UserCritsIndexView);
+
+	},
+	
+	events: {
+		'click #texts-tab':'showTexts',
+		'click #critiques-tab': 'showCritiques'
 	},
 	
 	tagName: 'div',
@@ -30,5 +35,48 @@ Wordshop.Views.UserShow = Backbone.CompositeView.extend({
 		this.$el.html(content);
 		this.attachSubviews();
 		return this;
+	},
+	
+	showTexts: function(event){
+		event.preventDefault();
+		
+		$('#texts-tab').addClass('active');
+		$('#profile-tab').removeClass('active');
+		$('#critiques-tab').removeClass('active');
+		
+		var that = this;
+		
+		if(this.subviews('#user-items-list')){
+			this.subviews('#user-items-list').forEach(function(subview){
+				that.removeSubview('#user-items-list', subview);
+			});
+		}
+		var UserTextsIndexView = new Wordshop.Views.UserTextsIndex({
+			collection: this.model.texts()
+		});
+
+		this.addSubview('#user-items-list', UserTextsIndexView);
+	},
+	
+	showCritiques: function(event){
+		event.preventDefault();
+
+		$('#critiques-tab').addClass('active');
+		$('#profile-tab').removeClass('active');
+		$('#texts-tab').removeClass('active');
+
+		var that = this;
+
+		if(this.subviews('#user-items-list')){
+			this.subviews('#user-items-list').forEach(function(subview){
+				that.removeSubview('#user-items-list', subview);
+			});
+		}
+		var UserCritsIndexView = new Wordshop.Views.UserCritsIndex({
+			collection: this.model.critiques()
+		});
+
+		this.addSubview('#user-items-list', UserCritsIndexView);
 	}
+
 });
